119b32dc6aac57931d6270793fdd51b840909374
Himanshu sharma <himanshusharmapune@gmail.com>
2025-12-16 20:18:43 +0530

    Pattern

---
+++ Lecture 4/numberpattern6.cpp
@@ -0,0 +1,26 @@
+/*
+A B C
+D E F 
+G H I
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    
+    int row=1;
+    char start = 'A';
+    while(row<=n){
+        int col = 1;
+        while(col<=n){
+            cout<<start;
+            start=start+1;
+            col=col+1;
+        }
+        cout<<endl;
+        row= row+1;
+    }
+}
